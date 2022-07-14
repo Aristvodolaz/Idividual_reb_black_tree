5b4ca7533422450fc559e63949423592c3af4af8
Aristvodolaz <kap.moral@mail.ru>
2022-07-14 15:39:03 +0300

    Thrid commits full prj with test

---
+++ Red_black_tree/RBTree.h
@@ -0,0 +1,37 @@
+#ifndef RBTREE_H_
+#define RBTREE_H_
+
+#include <vector>
+#include <iostream>
+#include <limits.h>
+#include <string.h>
+using namespace std;
+
+class Node {
+public:
+	const char* key;
+	Node* left;
+	Node* right;
+	Node* parent;
+	char color;
+	Node();
+	Node(const char*);
+};
+
+class RBTree {
+private:
+	Node* root;
+	Node* nil;
+
+public:
+	RBTree();
+	void leftRotate(Node*);
+	void rightRotate(Node*);
+	void treeInsert(Node*);
+	void RB_INSERT_FIXUP(Node*);
+	void RB_INSERT(Node*);
+	void INORDER();
+	void INORDER(Node*);
+};
+
+#endif
